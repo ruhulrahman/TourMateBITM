7ff904a2d1a20bdb9fac120e1467d9f4c6bb13e4
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-25 02:22:54 +0600

    LoginAcitivity few chanage

--- app/src/main/java/com/example/tourmate/LoginActivity.java
+++ app/src/main/java/com/example/tourmate/LoginActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+	private String name, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
